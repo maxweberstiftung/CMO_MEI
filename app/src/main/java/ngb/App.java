5095c49d36fdb26f3f58f4021b0b187621b80093
Jummit <jummit@web.de>
2021-08-24 19:53:37 +0200

    Show error dialog when no files are provided

--- app/src/main/java/ngb/App.java
+++ app/src/main/java/ngb/App.java
@@ -1,8 +1,17 @@
 package ngb;
 
+import javax.swing.JOptionPane;
+
 public class App {
     
     public static void main(String[] args) {
-
+        if (args.length == 0) {
+            showError("No files given");
+            return;
+        }
+    }
+    
+    public static void showError(String message) {
+        JOptionPane.showMessageDialog(null, message, "Error Processing Files", JOptionPane.WARNING_MESSAGE);
     }
 }
