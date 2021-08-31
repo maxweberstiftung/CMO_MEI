1e885a9ec21d7c14c609ec0b25e3aa4e766701b9
Thomas Weber <thomas.weber@notengrafik.com>
2021-08-31 09:42:35 +0200

    Add Transformer interface

---
+++ app/src/main/java/de/corpus_musicae_ottomanicae/mei/transform/Transformer.java
@@ -0,0 +1,11 @@
+package de.corpus_musicae_ottomanicae.mei.transform;
+
+import org.w3c.dom.Document;
+
+public interface Transformer {
+    /**
+     * An implementation may either return the modified input Document or
+     * generate a new Document as output.
+     */
+    public Document transform(Document input);
+}
