33f2053e3eb77635b9f32919bdf12922babcd5d1
Thomas Weber <thomas.weber@notengrafik.com>
2021-09-24 08:27:57 +0200

    Add encoding detection to XML parsing

--- app/src/main/java/de/corpus_musicae_ottomanicae/Xml.java
+++ app/src/main/java/de/corpus_musicae_ottomanicae/Xml.java
@@ -1,8 +1,14 @@
 package de.corpus_musicae_ottomanicae;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PushbackInputStream;
+import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
 
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -32,17 +38,31 @@ public class Xml {
         } else {
             klass = object.getClass();
         }
-        return parse(new InputStreamReader(klass.getResourceAsStream(fileName)));
+        return parse(klass.getResourceAsStream(fileName));
     }
 
     public static Document parse(String xmlString) throws SAXException, IOException, ParserConfigurationException {
-        return parse(new InputSource(new StringReader(xmlString)));
+        return parse(new StringReader(xmlString));
     }
 
-    public static Document parse(InputSource input) throws SAXException, IOException, ParserConfigurationException {
+    /**
+     * Detects the encoding (UTF-8, UTF-16LE, UTF-16BE) and parses the document.
+     */
+    public static Document parse(InputStream stream)
+            throws FileNotFoundException, UnsupportedEncodingException, SAXException, IOException {
+        PushbackInputStream pushbackStream = new PushbackInputStream(stream, 2);
+        String charsetName = guessEncoding(pushbackStream);
+        return parse(new InputStreamReader(pushbackStream, charsetName));
+    }
+
+    public static Document parse(Reader input) throws SAXException, IOException {
         DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
         builderFactory.setNamespaceAware(true);
-        return builderFactory.newDocumentBuilder().parse(input);
+        try {
+            return builderFactory.newDocumentBuilder().parse(new InputSource(input));
+        } catch (ParserConfigurationException e) {
+            throw new RuntimeException(e); // should be unreachable
+        }
     }
 
     public static String serialize(Node node) {
@@ -57,5 +77,45 @@ public class Xml {
             throw new RuntimeException(e);
         }
         return stringWriter.toString();
+    }
+
+    /**
+     * Consumes any existent BOM and returns an encoding string. Defaults to UTF-8,
+     * if no other encoding was plausible.
+     *
+     * @param stream PushbackInputStream with pushback buffer size of at least 2.
+     */
+    static String guessEncoding(PushbackInputStream stream) throws IOException {
+        byte[] bom = new byte[2];
+        if (stream.read(bom) < 2) {
+            throw new IOException("File is too short");
+        }
+        int bomValue = Byte.toUnsignedInt(bom[0]) * 256 + Byte.toUnsignedInt(bom[1]);
+        switch (bomValue) {
+            case 0xFFFE:
+                return "UTF-16LE";
+            case 0xFEFF:
+                return "UTF-16BE";
+            case 0xEFBB:
+                // UTF-8 BOM is EFBBBF
+                if (stream.read() != 0xBF) {
+                    throw new IOException("Unexpected leading bytes in file");
+                }
+                return "UTF-8";
+        }
+
+        // File has no (recognized) BOM.
+        // As we're working with XML, the first character must be ASCII (either
+        // whitespace or "<"), so in UTF-16BE the first byte will be 0 and in
+        // UTF-16LE, the second byte will be 0.
+        stream.unread(bom);
+        if (bom[0] == 0) {
+            return "UTF-16BE";
+        } else if (bom[1] == 0) {
+            return "UTF-16LE";
+        } else {
+            // Default to UTF-8
+            return "UTF-8";
+        }
     }
 }
