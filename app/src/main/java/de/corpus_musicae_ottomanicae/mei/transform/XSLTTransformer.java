75639facafdb662f281ba23442e10b80304418fb
Thomas Weber <thomas.weber@notengrafik.com>
2021-09-25 22:03:24 +0200

    XsltTransformer: Big exception handling improvement
    Differentiate between problems with input data and the transformations themselves

--- app/src/main/java/de/corpus_musicae_ottomanicae/mei/transform/XSLTTransformer.java
+++ app/src/main/java/de/corpus_musicae_ottomanicae/mei/transform/XSLTTransformer.java
@@ -1,43 +1,104 @@
 package de.corpus_musicae_ottomanicae.mei.transform;
 
+import java.util.ArrayList;
+
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.SourceLocator;
 import javax.xml.transform.dom.DOMSource;
 
 import org.w3c.dom.Document;
 
+import de.corpus_musicae_ottomanicae.mei.MeiInputException;
 import net.sf.saxon.s9api.DOMDestination;
+import net.sf.saxon.s9api.MessageListener2;
 import net.sf.saxon.s9api.Processor;
+import net.sf.saxon.s9api.QName;
 import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.XdmNode;
 import net.sf.saxon.s9api.Xslt30Transformer;
 import net.sf.saxon.s9api.XsltCompiler;
 import net.sf.saxon.s9api.XsltExecutable;
 
 public class XSLTTransformer implements Transformer {
+    static final QName meiInputErrorCode = new QName("MeiInputError");
 
-    private final Xslt30Transformer transformer;
+    static class ExceptionListener implements MessageListener2 {
+        public Exception exception = null;
+
+        @Override
+        public void message(XdmNode content, QName errorCode, boolean terminate, SourceLocator locator) {
+            if (!terminate) {
+                // QUESTION: Should this go to stderr?
+                System.out.println(content.toString());
+                return;
+            } else if (errorCode.equals(meiInputErrorCode)) {
+                try {
+                    exception = messageToMeiInputError(content);
+                } catch (TransformerException e) {
+                    exception = e;
+                }
+            } else {
+                exception = new TransformerException(content.getStringValue());
+            }
+        }
+
+        MeiInputException messageToMeiInputError(XdmNode message) throws TransformerException {
+            ArrayList<String> exceptionComponents = new ArrayList<>();
+            for (String localName : new String[] { "context", "message" }) {
+                var childIterator = message.children(localName).iterator();
+
+                if (!childIterator.hasNext()) {
+                    throw new TransformerException("Terminating XSLT message must have a " + localName + " element");
+                }
+                exceptionComponents.add(childIterator.next().getStringValue());
+                if (childIterator.hasNext()) {
+                    throw new TransformerException(
+                            "Terminating XSLT message must not have multiple " + localName + " elements");
+                }
+            }
+            return new MeiInputException(exceptionComponents.get(0), exceptionComponents.get(1));
+        }
+    }
+
+    private final XsltExecutable xsltExecutable;
     private static final Processor processor = new Processor(false);
     private static final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 
+    ExceptionListener exceptionListener = new ExceptionListener();
+
     public XSLTTransformer(Document xslt) throws SaxonApiException {
         XsltCompiler xsltCompiler = processor.newXsltCompiler();
-        XsltExecutable executable = xsltCompiler.compile(new DOMSource(xslt.getDocumentElement()));
-        transformer = executable.load30();
+        xsltExecutable = xsltCompiler.compile(new DOMSource(xslt.getDocumentElement()));
     }
 
     @Override
-    public Document transform(Document input) throws TransformerException {
+    public Document transform(Document input) throws TransformerException, MeiInputException {
+        ExceptionListener exceptionListener = new ExceptionListener();
+        Xslt30Transformer transformer = xsltExecutable.load30();
+        transformer.setMessageListener(exceptionListener);
+
         Document output;
         try {
             output = factory.newDocumentBuilder().newDocument();
         } catch (ParserConfigurationException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e); // should be unreachable
         }
+
         try {
             transformer.transform(new DOMSource(input), new DOMDestination(output));
         } catch (SaxonApiException e) {
-            throw new TransformerException(e);
+            if (exceptionListener.exception == null) {
+                throw new TransformerException(e);
+            } else if (exceptionListener.exception instanceof TransformerException) {
+                throw (TransformerException) exceptionListener.exception;
+            } else if (exceptionListener.exception instanceof MeiInputException) {
+                throw (MeiInputException) exceptionListener.exception;
+            } else {
+                throw new RuntimeException(exceptionListener.exception); // unreachable
+            }
         }
+
         return output;
     }
 }
--- app/src/test/java/de/corpus_musicae_ottomanicae/mei/transform/XSLTTransformerTest.java
+++ app/src/test/java/de/corpus_musicae_ottomanicae/mei/transform/XSLTTransformerTest.java
@@ -1,18 +1,49 @@
 package de.corpus_musicae_ottomanicae.mei.transform;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
 
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import de.corpus_musicae_ottomanicae.Xml;
+import de.corpus_musicae_ottomanicae.mei.MeiInputException;
+import net.sf.saxon.s9api.SaxonApiException;
 
 class XSLTTransformerTest {
+    static final Document dummyDocument;
+    static {
+        try {
+            dummyDocument = Xml.parse("<foo/>");
+        } catch (SAXException | IOException | ParserConfigurationException e) {
+            throw new RuntimeException();
+        }
+    }
+
     @Test
     void testTransform() throws Exception {
         Document xslt = Xml.loadResource(this, "xsl-transform-test.xsl");
         Document input = Xml.loadResource(this, "xsl-transform-test.xml");
         Document output = new XSLTTransformer(xslt).transform(input);
         assertEquals("out", output.getDocumentElement().getTextContent());
+    }
+
+    @Test
+    void testTransformerException() throws SaxonApiException, SAXException, IOException, ParserConfigurationException {
+        assertThrows(SaxonApiException.class, () -> new XSLTTransformer(dummyDocument));
+    }
+
+    @Test
+    void testMeiInputException() throws SAXException, IOException, SaxonApiException {
+        Document xslt = Xml.loadResource(this, "mei-input-exception-test.xsl");
+        Transformer transformer = new XSLTTransformer(xslt);
+        MeiInputException exception = assertThrows(MeiInputException.class, () -> transformer.transform(dummyDocument));
+        assertTrue(exception.getMessage().matches(".*test context.+test message.*"));
     }
 }
