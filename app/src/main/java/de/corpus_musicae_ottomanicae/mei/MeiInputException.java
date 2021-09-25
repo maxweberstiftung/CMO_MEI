205aa7ef180569b68dc3e7eb9ea4c95559cc9e9f
Thomas Weber <thomas.weber@notengrafik.com>
2021-09-25 21:58:05 +0200

    MeiInputException: Support elements outside of divisions and purely textual context descriptions
    XltTransformers can't return the context element, so they have to fallback to text

--- app/src/main/java/de/corpus_musicae_ottomanicae/mei/MeiInputException.java
+++ app/src/main/java/de/corpus_musicae_ottomanicae/mei/MeiInputException.java
@@ -2,17 +2,33 @@ package de.corpus_musicae_ottomanicae.mei;
 
 import org.w3c.dom.Element;
 
+/**
+ * Is thrown when the MEI input data is of a form that can not be processed,
+ * e.g. when elements that an operation relies on are missing or when attributes
+ * have unexpected values.
+ */
 public class MeiInputException extends Exception {
     public MeiInputException(Element context, String message) {
-        // As super() needs to be the first thing we call in the constructor,
-        // squeeze all the message construction into one line
-        super(contextInfo(context) + ": " + message);
+        this(contextInfo(context), message);
+    }
+
+    /**
+     * This constructor should only be used if no context element can be determined,
+     * especially when processing XSLTs where the terminating message will have to
+     * message the context in a textual way instead of handing on the element.
+     */
+    public MeiInputException(String context, String message) {
+        super(context + ": " + message);
     }
 
     private static String contextInfo(Element context) {
         String info = "";
 
-        info += "Division " + Util.contextDivisionNumber(context) + ", ";
+        try {
+            info += "Division " + Util.contextDivisionNumber(context) + ", ";
+        } catch (IllegalArgumentException e) {
+            // It's OK, `context` is just not an element occurring inside a division
+        }
         info += context.getTagName() + " element";
         if (context.hasAttribute("xml:id")) {
             info += " " + context.getAttribute("xml:id");
